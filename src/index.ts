d404898d660ba9da0505b8f49f107739201f973f
Jamie Pond <jamiepond259@gmail.com>
2026-01-30 17:17:13 -0800

    wip

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export {
+  parseChord,
+  formatChord,
+} from "./parseChord";
+
+export {
+  Note,
+  N,
+  toPitchClass,
+  ChordParseError,
+} from "./noteName";
+
+export type {
+  NoteName,
+  Letter,
+  Accidental,
+  TriadQuality,
+  ChordType,
+  Intervals,
+  ParsedChord,
+} from "./noteName";
